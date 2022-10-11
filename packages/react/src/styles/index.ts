d7395464a51dd8f357582ec9439dd10b7aef4e37
gabrielrsp <gabrielrsp18@gmail.com>
2022-10-10 23:49:54 -0300

    add stitches and storybook

---
+++ packages/react/src/styles/index.ts
@@ -0,0 +1,53 @@
+import {
+  colors,
+  fonts,
+  fontSizes,
+  fontWeights,
+  lineHeights,
+  radii,
+  space,
+} from '@ignite-ui/tokens'
+import { createStitches, defaultThemeMap } from '@stitches/react'
+
+// INTEGRAÇÃO DE TOKENS PARA CSS IN JS
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
+  themeMap: {
+    ...defaultThemeMap, // define o padrão de medidas para propriedades css
+    height: 'space', // para a propriedade height, use o token de space
+    width: 'space', // pro width também
+  },
+
+  theme: {
+    colors,
+    fontSizes,
+    fontWeights,
+    fonts,
+    lineHeights,
+    radii,
+    space,
+  },
+})
+
+/*
+THEME
+  - variaveis que serão utilizadas dentro da aplicação
+  - informa variaveis dos tipos que tenho na web
+  - no nosso arquivo de tokens, os nomes estão iguais 
+  - logo é so colar aqui, 
+  - então é ATRIBUÍDO ao stitches os nossos tokens!!!
+*/
+
+/*
+THEMEMAP
+  - Mapeia propriedades do css pra algum tipo de token
+
+*/
