a4130aa66d3163715c4783c13e0038f16eb25918
gabrielrsp <gabrielrsp18@gmail.com>
2022-10-16 23:16:18 -0300

    add text component

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nec ornare quam. Vestibulum dictum mi ex, quis rhoncus est fermentum quis.',
+  },
+} as Meta<TextProps>
+
+// Button variants
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
