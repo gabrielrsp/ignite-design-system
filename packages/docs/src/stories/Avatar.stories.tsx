73ffd7435109813df075b807217b0394bb0091a4
gabrielrsp <gabrielrsp18@gmail.com>
2022-10-17 23:35:44 -0300

    add avatar component

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/gabrielrsp.png',
+    alt: 'Gabriel Rodrigues',
+  },
+} as Meta<AvatarProps>
+
+// Button variants
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
