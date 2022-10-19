c3f3a6b37914f8e2f814d5c1bc51ba61982081ed
gabrielrsp <gabrielrsp18@gmail.com>
2022-10-18 22:21:51 -0300

    add button component

---
+++ packages/react/src/components/Button.tsx
@@ -0,0 +1,96 @@
+import { ComponentProps, ElementType } from 'react'
+
+import { styled } from '../styles'
+
+export const Button = styled('button', {
+  all: 'unset', // retira todas as propriedades padrão do botão (borda, background, etc)
+  borderRadius: '$sm',
+  fontSize: '$sm',
+  fontWeight: '$medium',
+  fontFamily: '$default',
+  textAlign: 'center',
+  minWidth: 128,
+  boxSizing: 'border-box',
+  padding: '0 $4',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  gap: '$2', // 2* 4 = 8px (caso haja um icone vai ter esse espaçamento entre ele e o texto)
+  cursor: 'pointer',
+
+  svg: {
+    width: '$4',
+    height: '$4',
+  },
+
+  '&:disabled': {
+    cursor: 'not-allowed',
+  },
+
+  variants: {
+    // variant é será da propriedade pra setar variante do button
+    variant: {
+      primary: {
+        color: '$white',
+        background: '$ignite500',
+
+        // quando  este botao não estiver desabilitado
+        '&:not(:disabled):hover': {
+          background: '$ignite300',
+        },
+
+        '&:disabled': {
+          backgroundColor: '$gray200',
+        },
+      },
+
+      secondary: {
+        color: '$ignite300',
+        border: '2px solid $ignite500',
+
+        '&:not(:disabled):hover': {
+          background: '$ignite500',
+          color: '$white',
+        },
+
+        '&:disabled': {
+          backgroundColor: '$gray200',
+          borderColor: '$gray200',
+        },
+      },
+
+      tertiary: {
+        color: '$gray100',
+
+        '$:not(:disabled):hover': {
+          color: '$white',
+        },
+
+        '&:disabled': {
+          color: '$gray600',
+          borderColor: '$gray200',
+        },
+      },
+    },
+
+    size: {
+      sm: {
+        height: 38,
+      },
+
+      md: {
+        height: 46,
+      },
+    },
+  },
+
+  defaultVariants: {
+    variant: 'primary',
+    size: 'md',
+  },
+})
+
+export interface ButtonProps extends ComponentProps<typeof Button> {
+  as?: ElementType
+}
