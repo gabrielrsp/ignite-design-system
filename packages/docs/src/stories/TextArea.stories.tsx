a974d4e2b9a4e9ac9ec9b79a415b043bafe23d30
gabrielrsp <gabrielrsp18@gmail.com>
2022-10-19 22:36:58 -0300

    add checkbox component

---
+++ packages/docs/src/stories/TextArea.stories.tsx
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Box, Text, TextArea, TextAreaProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Form/Text Area',
+  component: TextArea,
+  args: {},
+  decorators: [
+    // forma de customizar a view adicionando elementos extras que não sao filhos do componente
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+        >
+          <Text size="sm">Observations</Text>
+          {Story()}
+        </Box>
+      )
+    },
+  ],
+} as Meta<TextAreaProps>
+
+export const Primary: StoryObj<TextAreaProps> = {
+  args: {
+    placeholder: 'Add any observations...',
+  },
+}
+
+export const Disabled: StoryObj<TextAreaProps> = {
+  args: {
+    disabled: true,
+  },
+}
